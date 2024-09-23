68d1ecd6bda0dc67a065c6daefac9369db433352
umar-dev <umarmd2k22@gmail.com>
2024-09-23 13:27:54 +0530

    Add files via upload

---
+++ client/src/components/MinCategory.jsx
@@ -0,0 +1,37 @@
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Link } from "react-router-dom";
+
+const categories = [
+    "Shirts",
+    "Trousers",
+    "Jackets",
+    "Shoes",
+    "Kids Jackets",
+    "T-Shirts",
+    "Kids Shirts",
+    "Kids Shoes",
+    "Kids Lower",
+];
+
+const MinCategory = () => {
+    return (
+        <section className="hidden sm:block bg-white w-full px-2 sm:p-0 overflow-hidden border-b">
+            <div className="flex items-center justify-between p-0.5">
+                {categories.map((el, i) => (
+                    <Link
+                        to="/products"
+                        key={i}
+                        className="text-sm p-2 text-gray-800 font-medium hover:text-primaryBlue flex items-center gap-0.5 group"
+                    >
+                        {el}
+                        <span className="text-gray-400 group-hover:text-primaryBlue group-hover:rotate-180 transition-all ease-out">
+                            <ExpandMoreIcon sx={{ fontSize: "16px" }} />
+                        </span>
+                    </Link>
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default MinCategory;
